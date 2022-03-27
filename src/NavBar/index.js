754da9b1ed9ce127eabe021f5efaea00d734937b
asaad227 <a.saad227@yahoo.com>
2022-03-27 09:54:57 +0100

    pegion to pigeon

--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -1,17 +1,19 @@
-import Link from 'next/link';
-import React from 'react';
-import styles from "../../styles/navbar.module.css"
+import Link from "next/link";
+import React from "react";
+import styles from "../../styles/navbar.module.css";
 
 export default function NavBar() {
-  return <nav className={styles.mainNav}>
-<Link  href="/">
-    <a className={styles.home}>Home</a>
-</Link>
-<Link href="/facts">
-    <a className={styles.facts}>Pegion Facts</a>
-</Link>
-<Link href="/history">
-    <a className={styles.history}>Pegion History</a>
-</Link>
-  </nav>;
+  return (
+    <nav className={styles.mainNav}>
+      <Link href="/">
+        <a className={styles.home}>Home</a>
+      </Link>
+      <Link href="/facts">
+        <a className={styles.facts}>Pigeon Facts</a>
+      </Link>
+      <Link href="/history">
+        <a className={styles.history}>Pigeon History</a>
+      </Link>
+    </nav>
+  );
 }
